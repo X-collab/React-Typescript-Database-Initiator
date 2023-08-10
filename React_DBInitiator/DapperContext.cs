2e731492e73a2042046818569459b2ce0c74c9fd
Sibulele Mama <SibuleleM@hollywoodbets.net>
2023-08-10 11:36:14 +0200

    Add project files.

---
+++ React_DBInitiator/DapperContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace React_DBInitiator
+{
+    public class DapperContext
+    {
+        public IDbConnection CreateConnection()
+            => new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServerConnString"].ConnectionString);
+    }
+}
---
+++ React_DBInitiator/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Dapper;
+using React_DBInitiator;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+DapperContext context = new DapperContext();
+
+Console.WriteLine("");
+Console.WriteLine("Initiating creation of Data database....");
+
+Console.WriteLine("Creating Department Table....");
+
+try
+{
+    var query = "SELECT * FROM Department";
+
+    using (var connection = context.CreateConnection())
+    {
+        var creation = await connection.QueryAsync(query);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+//Insert creation method here
+
+Console.WriteLine("Database has been created successfully!!");
