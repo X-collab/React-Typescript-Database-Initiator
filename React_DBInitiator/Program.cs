5c69e7d2f749d8383c6b9e06269362a5a3b59feb
Sibulele Mama <SibuleleM@hollywoodbets.net>
2023-08-10 11:37:14 +0200

    Deletion of test connection

--- React_DBInitiator/Program.cs
+++ React_DBInitiator/Program.cs
@@ -13,20 +13,6 @@ Console.WriteLine("Initiating creation of Data database....");
 
 Console.WriteLine("Creating Department Table....");
 
-try
-{
-    var query = "SELECT * FROM Department";
-
-    using (var connection = context.CreateConnection())
-    {
-        var creation = await connection.QueryAsync(query);
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
 //Insert creation method here
 
 Console.WriteLine("Database has been created successfully!!");
